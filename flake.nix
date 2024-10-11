cecc4645af250fd99ddb16905cf13add9c000f7b
osbm <osmanfbayram@gmail.com>
2024-10-11 23:22:53 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "My system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        tartarus = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+      };
+    };
+  };
+}
