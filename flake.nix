6de30c0f0fe81a0bd578c9496dcfb25bc10f3d87
osbm <osmanfbayram@gmail.com>
2024-10-26 19:31:16 +0300

    add vscode server

--- flake.nix
+++ flake.nix
@@ -5,14 +5,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, ... }: {
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, vscode-server, ... }: {
     nixosConfigurations = {
       # revision = self.shortRev or self.dirtyShortRev or self.lastModified or "unknown";
       tartarus = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
-        modules = [ ./hosts/tartarus/configuration.nix ];
+        modules = [
+          ./hosts/tartarus/configuration.nix
+          vscode-server.nixosModules.default
+          ({ config, pkgs, ... }: {
+            services.vscode-server.enable = true;
+          })
+        ];
         specialArgs = {
           pkgs-unstable = import nixpkgs-unstable {
             inherit system;
