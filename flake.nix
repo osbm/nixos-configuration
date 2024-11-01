d7a7f2d1296fefafab1b3ffa89810982905744d2
osbm <osmanfbayram@gmail.com>
2024-11-01 15:41:46 +0300

    add sops-nix

--- flake.nix
+++ flake.nix
@@ -7,9 +7,12 @@
 
     vscode-server.url = "github:nix-community/nixos-vscode-server";
 
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, vscode-server, ... }: {
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, vscode-server, sops-nix, ... }: {
     nixosConfigurations = {
       # revision = self.shortRev or self.dirtyShortRev or self.lastModified or "unknown";
       tartarus = nixpkgs.lib.nixosSystem rec {
@@ -20,6 +23,7 @@
           ({ config, pkgs, ... }: {
             services.vscode-server.enable = true;
           })
+          sops-nix.nixosModules.sops
         ];
         specialArgs = {
           pkgs-unstable = import nixpkgs-unstable {
