1395a158cea7a60d130a7404530d06130f3dcb9f
osbm <osmanfbayram@gmail.com>
2024-11-25 17:13:36 +0300

    formatting flake

--- flake.nix
+++ flake.nix
@@ -11,35 +11,43 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    stylix.url = "github:danth/stylix/master";
-    stylix.inputs.nixpkgs.follows = "nixpkgs";
+    # stylix.url = "github:danth/stylix/master";
+    # stylix.inputs.nixpkgs.follows = "nixpkgs";
 
     dotfiles = {
-        url = "github:osbm/dotfiles";
-        flake = false;
+      url = "github:osbm/dotfiles";
+      flake = false;
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, vscode-server, sops-nix, stylix, ... }: {
-    nixosConfigurations = {
-      # revision = self.shortRev or self.dirtyShortRev or self.lastModified or "unknown";
-      tartarus = nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/tartarus/configuration.nix
-          vscode-server.nixosModules.default
-          sops-nix.nixosModules.sops
-          # stylix.nixosModules.stylix
-        ];
-        specialArgs = {
-          pkgs-unstable = import nixpkgs-unstable {
-            inherit system;
-            config.allowUnfree = true;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      vscode-server,
+      sops-nix,
+      ...
+    }:
+    {
+      nixosConfigurations = {
+        # revision = self.shortRev or self.dirtyShortRev or self.lastModified or "unknown";
+        tartarus = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/tartarus/configuration.nix
+            vscode-server.nixosModules.default
+            sops-nix.nixosModules.sops
+            # stylix.nixosModules.stylix
+          ];
+          specialArgs = {
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            system-label = self.shortRev or self.dirtyShortRev or self.lastModified or "unknown";
           };
-          system-label =
-            self.shortRev or self.dirtyShortRev or self.lastModified or "unknown";
         };
       };
     };
-  };
 }
