6365c99be20c0a4e5d9323f55dd0311358b04e67
osbm <osmanfbayram@gmail.com>
2024-12-02 16:31:44 +0300

    add ollama to only ymir

--- hosts/ymir/configuration.nix
+++ hosts/ymir/configuration.nix
@@ -139,6 +139,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    ollama
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
