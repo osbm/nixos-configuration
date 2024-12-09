610a2987bbfaea47ffe8f79818d4d8bf5ba59149
osbm <osmanfbayram@gmail.com>
2024-12-09 16:44:21 +0300

    use ollama service

--- hosts/ymir/configuration.nix
+++ hosts/ymir/configuration.nix
@@ -158,8 +158,12 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    ollama
   ];
+
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
