1b5e9b2e37aa540cae519a68d1970b5dd440518d
osbm <osmanfbayram@gmail.com>
2024-12-11 18:08:11 +0300

    try latest driver

--- hosts/ymir/configuration.nix
+++ hosts/ymir/configuration.nix
@@ -74,7 +74,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   # Enable the X11 windowing system.
