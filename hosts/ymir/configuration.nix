6f8ecf18b9013a5e78b5547f813f22cc63dcf87f
osbm <osmanfbayram@gmail.com>
2024-12-17 00:11:00 +0300

    dont just copy paste lines

--- hosts/ymir/configuration.nix
+++ hosts/ymir/configuration.nix
@@ -162,7 +162,7 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     nvidia-container-toolkit
-    khanelivim.packages.${system}.default
+    osbm-nvim.packages.${system}.default
   ];
 
   services.ollama = {
