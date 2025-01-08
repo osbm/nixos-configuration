0ee5ed2e84c095bf672857e85144bcc23886b9e4
osbm <osmanfbayram@gmail.com>
2025-01-08 17:36:46 +0300

    delete commented lines

--- flake.nix
+++ flake.nix
@@ -21,9 +21,6 @@
 
     agenix.url = "github:ryantm/agenix";
 
-    # stylix.url = "github:danth/stylix/master";
-    # stylix.inputs.nixpkgs.follows = "nixpkgs";
-
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
