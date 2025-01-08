7fb1550cbaccc5f5d187bf3188bc95339917b9dd
osbm <osmanfbayram@gmail.com>
2025-01-08 17:14:53 +0300

    block youtube

--- hosts/tartarus/configuration.nix
+++ hosts/tartarus/configuration.nix
@@ -21,12 +21,7 @@
     ../../common/concentration.nix
   ];
 
-  # stylix.enable = true;
-  # stylix.image = pkgs.fetchurl {
-  #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-  #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-  # };
-  # stylix.polarity = "dark";
+  concentration.blockYoutube = true;
 
   services.cloudflare-warp.enable = true;
 
