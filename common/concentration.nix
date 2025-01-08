a181a11e3efb33215a6e06ffbccd2a46fe577c52
osbm <osmanfbayram@gmail.com>
2025-01-08 17:12:01 +0300

    update concentration.nix

--- common/concentration.nix
+++ common/concentration.nix
@@ -4,11 +4,37 @@
   config,
   ...
 }: {
-  networking.extraHosts = ''
-    0.0.0.0   youtube.com
-    ::0       youtube.com
+  options = {
+    blockYoutube = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Disables youtube using /etc/hosts file";
+    };
+    blockTwitter = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Disables twitter using /etc/hosts file";
+    };
+  };
 
-    0.0.0.0   www.youtube.com
-    ::0       www.youtube.com
-  '';
+  config = lib.mkMerge [
+    (lib.mkIf config.blockYoutube {
+      networking.extraHosts = ''
+        0.0.0.0   youtube.com
+        ::0       youtube.com
+
+        0.0.0.0   www.youtube.com
+        ::0       www.youtube.com
+      '';
+    })
+    (lib.mkIf config.blockTwitter {
+      networking.extraHosts = ''
+        0.0.0.0   twitter.com
+        ::0       twitter.com
+
+        0.0.0.0   www.twitter.com
+        ::0       www.twitter.com
+      '';
+    })
+  ];
 }
