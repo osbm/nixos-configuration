69ab40797708e300fe7ca05aa2eecab8b6e25b14
osbm <osmanfbayram@gmail.com>
2025-01-13 20:40:29 +0300

    add option to arduino

---
+++ modules/arduino.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+
+
+  options = {
+    arduinoSetup = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Installs arduino-ide and adafruit-nrfutil and sets udev rules";
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf config.arduino {
+      environment.systemPackages = with pkgs; [
+        arduino-ide
+        adafruit-nrfutil
+        python3 # some arduino libraries require python3
+      ];
+
+      services.udev.extraRules = ''
+        KERNEL=="ttyUSB[0-9]*",MODE="0666"
+        KERNEL=="ttyACM[0-9]*",MODE="0666"
+      '';
+    })
+  ];
+}
