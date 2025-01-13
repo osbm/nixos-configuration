55564532328439307be9dc3cabea405477d3b362
osbm <osmanfbayram@gmail.com>
2025-01-13 22:24:09 +0300

    add ownership

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -4,11 +4,13 @@
     ssh-key-private = {
       file = ../secrets/ssh-key-private.age;
       path = "/home/osbm/.ssh/id_ed25519";
+      owner = "osbm";
       mode = "0600";
     };
     ssh-key-public = {
       file = ../secrets/ssh-key-public.age;
       path = "/home/osbm/.ssh/id_ed25519.pub";
+      owner = "osbm";
       mode = "0644";
     };
   };
