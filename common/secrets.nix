bdac402735b26056d06b1d005fb65e5e11499336
osbm <osmanfbayram@gmail.com>
2025-01-13 15:46:59 +0300

    add wifi pswd and ssid

--- common/secrets.nix
+++ common/secrets.nix
@@ -1,4 +1,6 @@
 {
   age.secrets = {
+    home-wifi-ssid = ../secrets/home-wifi-ssid.age;
+    home-wifi-password = ../secrets/home-wifi-password.age;
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,15 @@
 let
   ymir = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxc1ycxtzO2u4bHas71pi5CpR8Zzcj6GXjx1lLWMOHq";
+  tartarus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxbIyQnQFA1RFQKH4eHHWcT7Z0tCumerCsRMjlHgSPd";
+
+  machines = [
+    ymir
+    tartarus
+  ];
 in {
   ymir = ymir;
+  tartarus = tartarus;
+
+  "home-wifi-ssid.age".publicKeys = machines;
+  "home-wifi-password.age".publicKeys = machines;
 }
