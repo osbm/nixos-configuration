bcb68835398a0e21f001a441cdb56f64a804bdd5
osbm <osmanfbayram@gmail.com>
2025-01-14 00:51:38 +0300

    disable for now

--- modules/home.nix
+++ modules/home.nix
@@ -29,17 +29,17 @@
         hostname = "192.168.0.4";
         user = "osbm";
         identityFile = "/home/osbm/.ssh/id_ed25519";
-        extraOptions = {
-          "RemoteCommand" = "fish";
-        };
+        # extraOptions = {   # [ERROR] - (starship::print): Under a 'dumb' terminal (TERM=dumb).
+        #   "RemoteCommand" = "fish";
+        # };
       };
       "ymir" = {
         hostname = "192.168.0.2";
         user = "osbm";
         identityFile = "/home/osbm/.ssh/id_ed25519";
-        extraOptions = {
-          "RemoteCommand" = "fish";
-        };
+        # extraOptions = { # TODO fix the damn starship error
+        #   "RemoteCommand" = "fish";
+        # };
       };
     };
   };
