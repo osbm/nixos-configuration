28dc20aa36fcf28361465b408f466427d3deceb5
osbm <osmanfbayram@gmail.com>
2025-01-14 14:15:30 +0300

    try this for now

--- modules/home.nix
+++ modules/home.nix
@@ -24,22 +24,30 @@
     hashKnownHosts = true;
     compression = true;
 
+
+
     matchBlocks = {
       "tartarus" = {
         hostname = "192.168.0.4";
         user = "osbm";
         identityFile = "/home/osbm/.ssh/id_ed25519";
-        # extraOptions = {   # [ERROR] - (starship::print): Under a 'dumb' terminal (TERM=dumb).
-        #   "RemoteCommand" = "fish";
-        # };
+        setEnv = {
+          "TERM" = "xterm-256color";
+        };
+        extraOptions = {   # [ERROR] - (starship::print): Under a 'dumb' terminal (TERM=dumb).
+          "RemoteCommand" = "fish";
+        };
       };
       "ymir" = {
         hostname = "192.168.0.2";
         user = "osbm";
         identityFile = "/home/osbm/.ssh/id_ed25519";
-        # extraOptions = { # TODO fix the damn starship error
-        #   "RemoteCommand" = "fish";
-        # };
+        setEnv = {
+          "TERM" = "xterm-256color";
+        };
+        extraOptions = { # TODO fix the damn starship error
+          "RemoteCommand" = "fish";
+        };
       };
     };
   };
