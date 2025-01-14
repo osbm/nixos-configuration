75de8d117525f7320bef794aa378dcf151040e65
osbm <osmanfbayram@gmail.com>
2025-01-14 14:23:43 +0300

    tackle this later

--- modules/home.nix
+++ modules/home.nix
@@ -31,23 +31,23 @@
         hostname = "192.168.0.4";
         user = "osbm";
         identityFile = "/home/osbm/.ssh/id_ed25519";
-        setEnv = {
-          "TERM" = "xterm-256color";
-        };
-        extraOptions = {   # [ERROR] - (starship::print): Under a 'dumb' terminal (TERM=dumb).
-          "RemoteCommand" = "fish";
-        };
+        # setEnv = {
+        #   "TERM" = "xterm-256color";
+        # };
+        # extraOptions = {   # [ERROR] - (starship::print): Under a 'dumb' terminal (TERM=dumb).
+        #   "RemoteCommand" = "fish";
+        # };
       };
       "ymir" = {
         hostname = "192.168.0.2";
         user = "osbm";
         identityFile = "/home/osbm/.ssh/id_ed25519";
-        setEnv = {
-          "TERM" = "xterm-256color";
-        };
-        extraOptions = { # TODO fix the damn starship error
-          "RemoteCommand" = "fish";
-        };
+        # setEnv = {
+        #   "TERM" = "xterm-256color";
+        # };
+        # extraOptions = { # TODO fix the damn starship error
+        #   "RemoteCommand" = "fish";
+        # };
       };
     };
   };
