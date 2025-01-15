f778118fe5059ef1bd830dfb943925d7c62a2e7d
osbm <osmanfbayram@gmail.com>
2025-01-15 14:38:50 +0300

    secrets add pochita and rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,14 @@
 let
   ymir = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxc1ycxtzO2u4bHas71pi5CpR8Zzcj6GXjx1lLWMOHq";
   tartarus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxbIyQnQFA1RFQKH4eHHWcT7Z0tCumerCsRMjlHgSPd";
+  pochita = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICZIeS3r55uBJS1sXyVNlGsVY1wxqgy0lsoFBQo4ZD/F";
 
   osbm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfnV+qqUCJf92npNW4Jy0hIiepCJFBDJHXBHnUlNX0k";
 
   machines = [
     ymir
     tartarus
+    pochita
   ];
 in {
   "network-manager.age".publicKeys = machines ++ [osbm];
