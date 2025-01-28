f6decd434d084c39dd08ab247743973bee57f923
osbm <osmanfbayram@gmail.com>
2025-01-28 13:39:10 +0300

    is ip ok?

--- hosts/pochita/configuration.nix
+++ hosts/pochita/configuration.nix
@@ -16,7 +16,7 @@ in {
   nix.distributedBuilds = true;
   nix.buildMachines = [
     {
-      hostName = "ymir";
+      hostName = "192.168.0.2";
       systems = [ "x86_64-linux" "aarch64-linux" ];
       supportedFeatures = [ "big-parallel" "kvm"];
       sshKey = "/home/osbm/.ssh/id_ed25519";
