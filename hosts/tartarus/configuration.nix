86c92b9aa479a0f9d40c0fc4e8a1f58a690c264e
osbm <osmanfbayram@gmail.com>
2025-01-31 21:56:33 +0300

    enable tailscale on tartarus

--- hosts/tartarus/configuration.nix
+++ hosts/tartarus/configuration.nix
@@ -19,6 +19,8 @@ in {
 
   enableKDE = true;
 
+  enableTailscale = true;
+
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
   nix.settings.extra-platforms = config.boot.binfmt.emulatedSystems;
 
