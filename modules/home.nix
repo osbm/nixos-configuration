137572e1719e32af6e8a310a17e3db8fe98f89e7
osbm <osmanfbayram@gmail.com>
2025-02-04 01:19:38 +0300

    try enabling tmux this way

--- modules/home.nix
+++ modules/home.nix
@@ -54,6 +54,12 @@
       };
     };
   };
+  programs.tmux = {
+    enable = true;
+    historyLimit = 100000;
+    plugins = with pkgs; [
+    ];
+  };
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
