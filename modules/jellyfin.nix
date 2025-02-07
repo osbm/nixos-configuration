552fe8cc551a4192bb486fe8dc484beac80d9548
osbm <osmanfbayram@gmail.com>
2025-02-07 16:45:08 +0300

    change the user of jellyfin

--- modules/jellyfin.nix
+++ modules/jellyfin.nix
@@ -17,6 +17,9 @@
       services.jellyfin = {
         enable = true;
         openFirewall = true;
+        user = "osbm";
+        group = "users";
+        dataDir = "/home/osbm/.local/share/jellyfin";
       };
 
       networking.firewall.allowedTCPPorts = [8096];
