a6fccaf727faef9d0ecd6b1f450d18f7a2aca282
osbm <osmanfbayram@gmail.com>
2025-02-09 22:28:26 +0300

    update git config

--- home/git.nix
+++ home/git.nix
@@ -3,5 +3,34 @@
     enable = true;
     userEmail = "osmanfbayram@gmail.com";
     userName = "osbm";
+    ignores = [
+      "*.pyc" # python
+      "*.swp" # vim
+      "__pycache__" # python
+      ".DS_Store" # macOS
+      "result" # nix
+      "node_modules" # node
+    ];
+    extraConfig = {
+      credential = {
+        helper = "store";
+      };
+      core = {
+        editor = "vim";
+        pager = "cat";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      http = {
+        postBuffer = "1048576000";
+      };
+      https = {
+        postBuffer = "1048576000";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+    }
   };
 }
