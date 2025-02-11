f65e47231f301c822174ff6d9275933a031b82c7
osbm <osmanfbayram@gmail.com>
2025-02-12 01:20:18 +0300

    I pray this doesnt cause any trouble for me

--- modules/users.nix
+++ modules/users.nix
@@ -8,7 +8,7 @@
     osbm = {
       isNormalUser = true;
       description = "osbm";
-      initialPassword = "changeme";
+      initialHashedPassword = "$6$IamAbigfailure$irfkAsWev8CMAr78wUwUggclplXL98sbI21fpGY9nMDz47bU88RZWFLO7FcN5SdRA18ZSidkMqS76uLCMH68f.";
       extraGroups = ["networkmanager" "wheel" "docker"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfnV+qqUCJf92npNW4Jy0hIiepCJFBDJHXBHnUlNX0k"
@@ -19,10 +19,11 @@
     bayram = {
       isNormalUser = true;
       description = "bayram";
-      initialPassword = "changeme";
+      initialHashedPassword = "$6$IamAbigfailure$3BP231DVwbqUtZ.mq33nM/JitBrT2u26Y25VpsfBwhZbezMHz4XbySrOMnaMcCYdsb3wZFL3Ppcp0L.R8nonT.";
       extraGroups = ["networkmanager"];
       packages = with pkgs; [
       ];
     };
+    root.initialHashedPassword = "$6$IamAbigfailure$irfkAsWev8CMAr78wUwUggclplXL98sbI21fpGY9nMDz47bU88RZWFLO7FcN5SdRA18ZSidkMqS76uLCMH68f.";
   };
 }
