432b607d2c26db274076aa35b53997c54d4ce491
osbm <osmanfbayram@gmail.com>
2025-02-23 23:52:41 +0300

    fish?

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,51 +1,30 @@
-{pkgs, ...}: {
+{...}:
+let
+  # define a block that just takes a hostname and returns attrset to not repeat the same fields
+  sshBlock = hostname: {
+    hostname = hostname;
+    user = "osbm";
+    identityFile = "/home/osbm/.ssh/id_ed25519";
+    extraOptions = { # [ERROR] - (starship::print): Under a 'dumb' terminal (TERM=dumb).
+      "RemoteCommand" = "fish";
+      "RequestTTY" = "force";
+    };
+  };
+in
+{
   programs.ssh = {
     enable = true;
     hashKnownHosts = true;
     compression = true;
     matchBlocks = {
-      "tartarus" = {
-        hostname = "192.168.0.4";
-        user = "osbm";
-        identityFile = "/home/osbm/.ssh/id_ed25519";
-        # setEnv = {
-        #   "TERM" = "xterm-256color";
-        # };
-        # extraOptions = {   # [ERROR] - (starship::print): Under a 'dumb' terminal (TERM=dumb).
-        #   "RemoteCommand" = "fish";
-        # };
-      };
-      "ymir" = {
-        hostname = "192.168.0.2";
-        user = "osbm";
-        identityFile = "/home/osbm/.ssh/id_ed25519";
-        # setEnv = {
-        #   "TERM" = "xterm-256color";
-        # };
-        # extraOptions = { # TODO fix the damn starship error
-        #   "RemoteCommand" = "fish";
-        # };
-      };
-      "pochita" = {
-        hostname = "192.168.0.9";
-        user = "osbm";
-        identityFile = "/home/osbm/.ssh/id_ed25519";
-      };
-      "pochita-tailscale" = {
-        hostname = "pochita.curl-boga.ts.net";
-        user = "osbm";
-        identityFile = "/home/osbm/.ssh/id_ed25519";
-      };
-      "ymir-tailscale" = {
-        hostname = "ymir.curl-boga.ts.net";
-        user = "osbm";
-        identityFile = "/home/osbm/.ssh/id_ed25519";
-      };
-      "tartarus-tailscale" = {
-        hostname = "tartarus.curl-boga.ts.net";
-        user = "osbm";
-        identityFile = "/home/osbm/.ssh/id_ed25519";
-      };
+      ymir = sshBlock "192.168.0.2";
+      ymir-ts = sshBlock "ymir.curl-boga.ts.net";
+      atreus = sshBlock "192.168.0.3";
+      atreus-ts = sshBlock "atreus.curl-boga.ts.net";
+      tartarus = sshBlock "192.168.0.4";
+      tartarus-ts = sshBlock "tartarus.curl-boga.ts.net";
+      pochita = sshBlock "192.168.0.9";
+      pochita-ts = sshBlock "pochita.curl-boga.ts.net";
     };
   };
 }
