7f67da24217c0de12e39b2dee7b2a8d37ed20866
osbm <osmanfbayram@gmail.com>
2025-02-26 00:53:41 +0300

    networking with secrets is hard in nixos

--- hosts/harmonica/configuration.nix
+++ hosts/harmonica/configuration.nix
@@ -28,66 +28,17 @@
 
   networking.hostName = "harmonica";
 
-  # networking = {
-  #   interfaces."wlan0".useDHCP = true;
-  #   wireless = {
-  #     enable = true;
-  #     interfaces = ["wlan0"];
-  #     # ! Change the following to connect to your own network
-  #     networks = {
-  #       "${config.age.secrets.home-wifi-ssid.}" = {
-  #         psk = "${secrets.home-wifi-password.age}";
-  #       };
-  #     };
-  #   };
-  # };
-
-  networking.networkmanager.ensureProfiles = {
-    environmentFiles = [
-      config.age.secrets.nm-secrets.path
-    ];
-
-    profiles = {
-      House_Bayram = {
-        connection = {
-          id = "House_Bayram";
-          type = "wifi";
+  networking = {
+    interfaces."wlan0".useDHCP = true;
+    wireless = {
+      enable = true;
+      interfaces = ["wlan0"];
+      networks = {
+        "House_Bayram" = {
+          psk = "PASSWORD";
         };
-        ipv4 = {
-          method = "auto";
-        };
-        ipv6 = {
-          addr-gen-mode = "stable-privacy";
-          method = "auto";
-        };
-        wifi = {
-          mode = "infrastructure";
-          ssid = "House_Bayram";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$HOME_WIFI";
-        };
-      };
-      it_hurts_when_IP = {
-        connection = {
-          id = "it_hurts_when_IP";
-          type = "ethernet";
-        };
-        ipv4 = {
-          method = "auto";
-        };
-        ipv6 = {
-          addr-gen-mode = "stable-privacy";
-          method = "auto";
-        };
-        wifi = {
-          mode = "infrastructure";
-          ssid = "it_hurts_when_IP";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$MOBILE_HOTSPOT";
+        "it_hurts_when_IP" = {
+          psk = "PASSWORD";
         };
       };
     };
