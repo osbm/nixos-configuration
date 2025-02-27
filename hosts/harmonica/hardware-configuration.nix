62ab5d54f1b967d494b184722035f597e6e4d0ed
osbm <osmanfbayram@gmail.com>
2025-02-27 19:31:50 +0300

    formatting

--- hosts/harmonica/hardware-configuration.nix
+++ hosts/harmonica/hardware-configuration.nix
@@ -16,12 +16,10 @@
     algorithm = "zstd";
   };
 
-
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
   };
-
 
   hardware = {
     enableRedistributableFirmware = lib.mkForce false;
